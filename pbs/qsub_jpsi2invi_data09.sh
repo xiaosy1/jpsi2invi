e69463cf4e00fcacf7d193ff3b6145e3f89c8052
SHI Xin <shixin@ihep.ac.cn>
2016-09-03 15:45:31 +0800

    data90 run

---
+++ pbs/qsub_jpsi2invi_data09.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to submit PBS jobs 
+# Author: SHI Xin <shixin@ihep.ac.cn>
+# Date: [2016-09-02 Fri 10:37] 
+
+#PBS -N jdata09
+#PBS -q "besq@torqsrv"
+#PBS -o $HOME/bes/jpsi2invi/v0.1/run/log/data09/jpsi2invi_data09.log 
+#PBS -t 1-314%200
+
+
+date
+
+hostname
+
+cd $HOME/bes/jpsi2invi/v0.1/
+source setup.sh 
+cd pbs 
+boss.exe jobOptions_jpsi2invi_data09.pbs
+
+date
+
+
