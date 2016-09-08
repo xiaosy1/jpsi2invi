7dd2e326277c56a9d89668bb27a1aa1a3bb7efb7
Xin Shi <Xin.Shi@outlook.com>
2016-09-08 11:10:30 +0100

    clean for data only

--- python/plt_summary.py
+++ python/plt_summary.py
@@ -17,204 +17,244 @@ def main():
     set_root_style(stat=0, grid=0) 
     ROOT.gStyle.SetPadLeftMargin(0.15)
 
-    c = ROOT.TCanvas('c', 'c', 800, 800)    
+    sample = sys.argv[1:]
+    fs = get_files_from_sample(sample)
+    c = ROOT.TCanvas('c', 'c', 800, 800)
 
-    f1 = ROOT.TFile('run/hist/data/jpsi2invi_data_merged_1.root')
-    f2 = ROOT.TFile('run/hist/mc_psip12/jpsi2invi_mc_psip_12mc_merged_1.root')
-    f3 = ROOT.TFile('run/hist/con3650/jpsi2invi_con3650_merged_1.root')
-
-    fs = [f1, f2, f3]
+    draw_mrecpipi(sample, c, fs)
+    draw_mpipi(sample, c, fs)
+    draw_pip_p(sample, c, fs) 
+    draw_pim_p(sample, c, fs) 
+    draw_pip_costhe(sample, c, fs) 
+    draw_pim_costhe(sample, c, fs)
+    draw_cospipi(sample, c, fs) 
+    draw_cos2pisys(sample, c, fs)
+    draw_ngam(sample, c, fs)
     
-    draw_mrecpipi(c, fs)
-    draw_mpipi(c, fs)
-    draw_pip_p(c, fs) 
-    draw_pim_p(c, fs) 
-    draw_pip_costhe(c, fs) 
-    draw_pim_costhe(c, fs)
-    draw_cospipi(c, fs) 
-    draw_cos2pisys(c, fs)
-    draw_ngam(c, fs) 
+
+def get_files_from_sample(sample):
+    fs = [] 
+    if 'data' in sample:
+        fs.append(ROOT.TFile('run/hist/data/jpsi2invi_data_merged_1.root'))
+        
+    #f1 = ROOT.TFile('run/hist/data/jpsi2invi_data_merged_1.root')
+    # f1 = ROOT.TFile('run/hist/data09/jpsi2invi_data09_merged_1.root')
+    # f2 = ROOT.TFile('run/hist/mc_psip12/jpsi2invi_mc_psip_12mc_merged_1.root')
+    # f3 = ROOT.TFile('run/hist/con3650/jpsi2invi_con3650_merged_1.root')
+    # fs = [f1, f2, f3]
+    return fs 
+
 
 def get_common_objects_to_draw(fs, hname, leg):
-    h1 = fs[0].Get(hname)
-    h1.Sumw2()
-    h1.GetXaxis().SetLabelSize(0.03) 
-    h1.GetYaxis().SetLabelSize(0.03) 
-    h1.GetYaxis().SetTitleOffset(1.8) 
-    h1.SetMarkerStyle(ROOT.kFullDotLarge)
-
-    h2 = fs[1].Get(hname)
-    h2.SetLineColor(29)
-    h2.SetFillColor(29)
-
-    h3 = fs[2].Get(hname)
-    h3.SetLineColor(ROOT.kOrange)
-    h3.SetFillColor(ROOT.kOrange)
+    hs = []
 
     leg.SetTextSize(0.03)
     leg.SetFillColor(0)
     leg.SetFillStyle(0)
     leg.SetLineColor(0)
     leg.SetShadowColor(0)
-    leg.AddEntry(h1, "Data","lp")
-    leg.AddEntry(h2, "#psi(2S) inclusive MC")
-    leg.AddEntry(h3, "Cont.")
+    
+    for f in fs:
+        h = f.Get(hname)
+        if fs.index(f) == 0:
+            h.Sumw2()
+            h.GetXaxis().SetLabelSize(0.03) 
+            h.GetYaxis().SetLabelSize(0.03) 
+            h.GetYaxis().SetTitleOffset(1.8) 
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            leg.AddEntry(h, "Data", "lp")
+            
+        elif fs.index(f) == 1:
+            h.SetLineColor(29)
+            h.SetFillColor(29)
+            leg.AddEntry(h, "#psi(2S) inclusive MC")
 
-    hs = [h1, h2, h3]
+        elif fs.index(f) == 2:
+            h.SetLineColor(ROOT.kOrange)
+            h.SetFillColor(ROOT.kOrange)
+            leg.AddEntry(h3, "Cont.")
+            
+        hs.append(h) 
+            
     return  hs, leg 
 
 
-def draw_mrecpipi(c, fs):
+def draw_mrecpipi(sample, c, fs):
     hname = 'h_mrecpipi'
-    leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
-    hs, leg = get_common_objects_to_draw(fs, hname, leg) 
-
-    hs[0].SetXTitle('M(recoil(#pi^{+}#pi^{-})) (GeV/c^{2})') 
-    hs[0].SetYTitle('Events/(0.0012 GeV/c^{2})')
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
-    leg.Draw()
-    
-    figfile = 'doc/fig/jpsi2invi_mrecpipi.pdf'
+    figfile = 'doc/fig/jpsi2invi_mrecpipi_%s.pdf' %'_'.join(sample)
     check_outfile_path(figfile)
+        
+    leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
+    hs, leg = get_common_objects_to_draw(fs, hname, leg) 
+
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('M(recoil(#pi^{+}#pi^{-})) (GeV/c^{2})') 
+            h.SetYTitle('Events/(0.0012 GeV/c^{2})')
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else:
+            h.Draw('same')
+
+    leg.Draw()
     c.SaveAs(figfile)
 
-
-def draw_mpipi(c, fs):
+    
+def draw_mpipi(sample, c, fs):
     hname = 'h_mpipi'
+    figfile = 'doc/fig/jpsi2invi_mpipi_%s.pdf' %'_'.join(sample) 
+        
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
     
-    hs[0].SetXTitle('M(#pi^{+}#pi^{-}) (GeV/c^{2})') 
-    hs[0].SetYTitle('Events/(0.0012 GeV/c^{2})')
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
-    leg.Draw()
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('M(#pi^{+}#pi^{-}) (GeV/c^{2})') 
+            h.SetYTitle('Events/(0.0012 GeV/c^{2})')
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else:
+            h.Draw('same')
 
-    figfile = 'doc/fig/jpsi2invi_mpipi.pdf'
+    leg.Draw()
     c.SaveAs(figfile)
 
 
-def draw_pip_p(c, fs):
-    hname = 'h_pip_p' 
+def draw_pip_p(sample, c, fs):
+    hname = 'h_pip_p'
+    figfile = 'doc/fig/jpsi2invi_pip_p_%s.pdf'  %'_'.join(sample) 
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
 
-    hs[0].SetXTitle('P(#pi^{+}) (GeV/c)')
-    hs[0].SetYTitle('Events/(0.005 GeV/c)') 
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('P(#pi^{+}) (GeV/c)')
+            h.SetYTitle('Events/(0.005 GeV/c)') 
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else:
+            h.Draw('same') 
+
     leg.Draw()
-    
-    figfile = 'doc/fig/jpsi2invi_pip_p.pdf'
     c.SaveAs(figfile)
 
 
-def draw_pim_p(c, fs):
+def draw_pim_p(sample, c, fs):
     hname = 'h_pim_p'
+    figfile = 'doc/fig/jpsi2invi_pim_p_%s.pdf'  %'_'.join(sample) 
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
     
-    hs[0].SetXTitle('P(#pi^{-}) (GeV/c)')
-    hs[0].SetYTitle('Events/(0.005 GeV/c)') 
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
-    leg.Draw()
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('P(#pi^{-}) (GeV/c)')
+            h.SetYTitle('Events/(0.005 GeV/c)') 
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else: 
+            h.Draw('same') 
 
-    figfile = 'doc/fig/jpsi2invi_pim_p.pdf'
+    leg.Draw()
     c.SaveAs(figfile)
 
 
-def draw_pip_costhe(c, fs):
+def draw_pip_costhe(sample, c, fs):
     hname = 'h_pip_costhe'
+    figfile = 'doc/fig/jpsi2invi_pip_costhe_%s.pdf'   %'_'.join(sample)  
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
     
-    hs[0].SetXTitle('cos#theta_{#pi^{+}}')
-    hs[0].SetYTitle('Events/0.02')
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
-    leg.Draw()
+    i = 0
+    for h in hs:
+        i = i+1 
+        if i == 1:
+            h.SetXTitle('cos#theta_{#pi^{+}}')
+            h.SetYTitle('Events/0.02')
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else:
+            h.Draw('same') 
 
-    figfile = 'doc/fig/jpsi2invi_pip_costhe.pdf'
+    leg.Draw()
     c.SaveAs(figfile)
 
 
-def draw_pim_costhe(c, fs):
+def draw_pim_costhe(sample, c, fs):
     hname = 'h_pim_costhe'
+    figfile = 'doc/fig/jpsi2invi_pim_costhe_%s.pdf' %'_'.join(sample)  
+    
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
     
-    hs[0].SetXTitle('Cos#theta_{#pi^{-}}')
-    hs[0].SetYTitle('Events/0.02')
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
-    leg.Draw()
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('Cos#theta_{#pi^{-}}')
+            h.SetYTitle('Events/0.02')
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else: 
+            h.Draw('same') 
 
-    figfile = 'doc/fig/jpsi2invi_pim_costhe.pdf'
+    leg.Draw()
     c.SaveAs(figfile)
 
 
-def draw_cospipi(c, fs):
+def draw_cospipi(sample, c, fs):
     hname = 'h_cospipi'
+    figfile = 'doc/fig/jpsi2invi_cospipi_%s.pdf' %'_'.join(sample) 
+    
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
     
-    hs[0].SetXTitle('cos#theta_{#pi^{+}#pi^{-}}')
-    hs[0].SetYTitle('Events/0.01')
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
-    leg.Draw()
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('cos#theta_{#pi^{+}#pi^{-}}')
+            h.SetYTitle('Events/0.01')
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else: 
+            h.Draw('same') 
 
-    figfile = 'doc/fig/jpsi2invi_cospipi.pdf'
+    leg.Draw()
     c.SaveAs(figfile)
 
 
-def draw_cos2pisys(c, fs):
+def draw_cos2pisys(sample, c, fs):
     hname = 'h_cos2pisys'
+    figfile = 'doc/fig/jpsi2invi_cos2pisys_%s.pdf' %'_'.join(sample)     
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
 
-    hs[0].SetXTitle('Cos#theta_{#pi#pi sys.}')
-    hs[0].SetYTitle('Events/0.02')
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
-    leg.Draw()
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('Cos#theta_{#pi#pi sys.}')
+            h.SetYTitle('Events/0.02')
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else:
+            h.Draw('same') 
 
-    figfile = 'doc/fig/jpsi2invi_cos2pisys.pdf'
+    leg.Draw()
     c.SaveAs(figfile)
 
 
-def draw_ngam(c, fs):
+def draw_ngam(sample, c, fs):
     hname = 'h_ngam'
+    figfile = 'doc/fig/jpsi2invi_ngam_%s.pdf' %'_'.join(sample)  
+    
     leg = ROOT.TLegend(0.2, 0.71, 0.32, 0.91)
     hs, leg = get_common_objects_to_draw(fs, hname, leg) 
 
-    hs[0].SetXTitle('N_{#gamma}')
-    hs[0].SetYTitle('Events')
-    hs[0].SetMarkerStyle(ROOT.kFullDotLarge)
-    hs[0].Draw()
-    hs[1].Draw('same') 
-    hs[2].Draw('same') 
+    for h in hs:
+        if hs.index(h) == 0:
+            h.SetXTitle('N_{#gamma}')
+            h.SetYTitle('Events')
+            h.SetMarkerStyle(ROOT.kFullDotLarge)
+            h.Draw()
+        else: 
+            h.Draw('same') 
+
     leg.Draw()
-    
-    figfile = 'doc/fig/jpsi2invi_ngam.pdf'
     c.SaveAs(figfile)
 
     
