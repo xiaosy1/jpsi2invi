5f4db50f58c8f55e795ac71217378e2e3f42ca66
Suyu Xiao <1203111345@qq.com>
2017-10-24 17:27:22 +0800

    update

--- submit.sh
+++ submit.sh
@@ -13,30 +13,30 @@ usage() {
     printf "\n\t%-9s  %-40s"  "0.1"      "[run data sample]" 
     printf "\n\t%-9s  %-40s"  "0.1.1"    "Run with a few samples" 
     printf "\n\t%-9s  %-40s"  "0.1.2"    "Split data sample with each group 20G"
-    printf "\n\t%-9s  %-40s"  "0.1.3"    "Submit PBS jobs on data"
-    printf "\n\t%-9s  %-40s"  "0.1.4"    "Check PBS jobs on data."
+    printf "\n\t%-9s  %-40s"  "0.1.3"    "Submit Condor jobs on data"
+    printf "\n\t%-9s  %-40s"  "0.1.4"    "Check Condor jobs on data."
     printf "\n\t%-9s  %-40s"  "0.1.5"    "Select events."
     printf "\n\t%-9s  %-40s"  "0.1.6"    "Submit events selection jobs on data."
     printf "\n\t%-9s  %-40s"  "0.1.7"    "Check events jobs on data."
     printf "\n\t%-9s  %-40s"  "0.1.8"    "Merge events files." 
     printf "\n\t%-9s  %-40s"  "0.1.9"    "Plot summary with data."
     printf "\n\t%-9s  %-40s"  "0.1.10"   "Split con3650 data sample with each group 20G"
-    printf "\n\t%-9s  %-40s"  "0.1.11"   "Submit PBS jobs on con3650 data"
-    printf "\n\t%-9s  %-40s"  "0.1.12"   "Check PBS jobs on con3650 data"
+    printf "\n\t%-9s  %-40s"  "0.1.11"   "Submit Condor jobs on con3650 data"
+    printf "\n\t%-9s  %-40s"  "0.1.12"   "Check Condor jobs on con3650 data"
     printf "\n\t%-9s  %-40s"  "0.1.13"   "Select events on con3650 data"
     printf "\n\t%-9s  %-40s"  "0.1.14"   "Merge events on con3650 data"
     printf "\n\t%-9s  %-40s"  "0.1.15"   "Plot summary with data and con3650"
     printf "\n\t%-9s  %-40s"  "0.1.16"   "Split data09 sample with each group 20G"
-    printf "\n\t%-9s  %-40s"  "0.1.17"   "Submit PBS jobs on 09 data sample"
-    printf "\n\t%-9s  %-40s"  "0.1.18"   "Check PBS PBS jobs on 09 data sample"
+    printf "\n\t%-9s  %-40s"  "0.1.17"   "Submit Condor jobs on 09 data sample"
+    printf "\n\t%-9s  %-40s"  "0.1.18"   "Check Condor PBS jobs on 09 data sample"
     printf "\n\t%-9s  %-40s"  "0.1.19"   "Event selecion on 09 data"
     printf "\n\t%-9s  %-40s"  "0.1.20"   "PBS jobs for event selecion on 09 data"
-    printf "\n\t%-9s  %-40s"  "0.1.21"   "Check PBS jobs for event selecion on 09 data"
+    printf "\n\t%-9s  %-40s"  "0.1.21"   "Check Condor jobs for event selecion on 09 data"
     printf "\n\t%-9s  %-40s"  "0.1.22"   "Merge event file on 09 data"
     printf "\n\t%-9s  %-40s"  "0.1.23"   "Plot summary on 09 data"
     printf "\n\t%-9s  %-40s"  "0.1.24"   "Run with Jpsi2incl on a few 09 data events "
-    printf "\n\t%-9s  %-40s"  "0.1.25"   "Submit Jpsi2incl PBS jobs on data09"
-    printf "\n\t%-9s  %-40s"  "0.1.26"   "Check Jpsi2incl PBS jobs on data09"
+    printf "\n\t%-9s  %-40s"  "0.1.25"   "Submit Jpsi2incl Condor jobs on data09"
+    printf "\n\t%-9s  %-40s"  "0.1.26"   "Check Jpsi2incl Condor jobs on data09"
     printf "\n\t%-9s  %-40s"  "0.1.27"   "Select Jpsi2incl events on data09"
     printf "\n\t%-9s  %-40s"  ""         ""
     printf "\n\t%-9s  %-40s"  "0.2"      "[run on MC sample]"
@@ -80,7 +80,7 @@ case $option in
 	 ;;
 
     0.1.1) echo "Run with a few events ..."
-	   boss.exe pbs/jobOptions_jpsi2invi.txt
+	   boss.exe condor/jobOptions_jpsi2invi.txt
 	   ;;
     
     0.1.2) echo "Split data sample with each group 20G ..."
@@ -88,14 +88,16 @@ case $option in
 	   # made 633 groups 
 	   ;;
 
-    0.1.3) echo "Submit PBS jobs on data..."
+    0.1.3) echo "Submit Condor jobs on data..."
 	   mkdir run/data
-	   mkdir run/log/data  
-	   qsub pbs/qsub_jpsi2invi_data.sh  
+	   mkdir run/log/data
+	   hep_sub -g physics -o /besfs/groups/nphy/users/xiaosy/run/run/tmp20171023/log.tex -e /besfs/groups/nphy/users/xiaosy/run/run/tmp20171023/err.txt /afs/ihep.ac.cn/users/x/xiaosy/bes/jpsi2invi/v0.1/condor/jpsi2invi/jpsi2invi_data.sh
+	   #qsub pbs/qsub_jpsi2invi_data.sh  
 	   ;;
 
-    0.1.4) echo "Check PBS jobs on data..."
-	   ./python/chk_pbsjobs.py $HOME/bes/jpsi2invi/v0.1/run/data  633
+    0.1.4) echo "Check Condor jobs on data..."
+	   #./python/chk_condorjobs.py /besfs/groups/nphy/users/xiaosy/run/run/tmp20171023 633
+	   ./python/chk_condorjobs.py $HOME/bes/jpsi2invi/v0.1/run/data  633
 	   ;;
     
     0.1.5) echo  "Select events on data..."
@@ -103,13 +105,13 @@ case $option in
 	   ./python/sel_events.py  run/data/jpsi2invi_data-1.root  run/events/data/jpsi2invi_data-1.root 
 	   ;; 
 
-    0.1.6) echo "Submit selection PBS jobs on data..."
+    0.1.6) echo "Submit selection Condor jobs on data..."
 	   mkdir run/events/data
 	   mkdir run/log/events/data   
 	   qsub pbs/qsub_jpsi2invi_events_data.sh  
 	   ;;
 
-    0.1.7) echo "Check PBS jobs on events data..."
+    0.1.7) echo "Check Condor jobs on events data..."
 	   ./python/chk_pbsjobs.py run/events/data  633
 	   ;;
 
@@ -127,13 +129,13 @@ case $option in
 	    # made 11 groups 
 	    ;;
 
-    0.1.11) echo "Submit PBS jobs on con3650 data..."
+    0.1.11) echo "Submit Condor jobs on con3650 data..."
 	    mkdir run/con3650
 	    mkdir run/log/con3650  
 	    qsub pbs/qsub_jpsi2invi_con3650.sh  
 	    ;;
 
-    0.1.12) echo "Check PBS jobs on con3650 data..."
+    0.1.12) echo "Check Condor jobs on con3650 data..."
 	   ./python/chk_pbsjobs.py run/con3650  11 
 	   ;;
 
@@ -159,13 +161,13 @@ case $option in
 	   ./python/get_samples.py /bes3fs/offline/data/664-1/psip/dst $HOME/bes/jpsi2invi/v0.1/run/samples/data_664-1_psip.txt 20G
 	   # made 314 groups 
 	   ;;
-    0.1.17) echo "Submit PBS jobs on 09 data..."
+    0.1.17) echo "Submit Condor jobs on 09 data..."
 	    mkdir run/data09
 	    mkdir -p run/log/data09  
 	    qsub pbs/qsub_jpsi2invi_data09.sh  
 	    ;;
 
-    0.1.18) echo "Check PBS jobs on 09 data..."
+    0.1.18) echo "Check Condor jobs on 09 data..."
 	   ./python/chk_pbsjobs.py run/data09  314 
 	   ;;
 
@@ -174,13 +176,13 @@ case $option in
 	   ./python/sel_events.py  run/data09/jpsi2invi_data09-1.root  run/events/data09/jpsi2invi_data09-1.root 
 	   ;; 
 
-    0.1.20) echo "PBS jobs for event selection on 09 data..."
+    0.1.20) echo "Condor jobs for event selection on 09 data..."
 	   mkdir run/events/data09
 	   mkdir run/log/events/data09   
 	   qsub pbs/qsub_jpsi2invi_events_data09.sh  
 	   ;;
 
-    0.1.21) echo "Check PBS jobs of event selection on 09 data..."
+    0.1.21) echo "Check Condor jobs of event selection on 09 data..."
 	   ./python/chk_pbsjobs.py run/events/data09  314
 	   ;;
 
@@ -197,13 +199,13 @@ case $option in
 	   boss.exe pbs/jobOptions_jpsi2incl.txt
 	   ;;
 
-    0.1.25) echo "Submit Jpsi2incl PBS jobs on data09..."
+    0.1.25) echo "Submit Jpsi2incl Condor jobs on data09..."
 	   mkdir -p run/jpsi2incl/data09
 	   mkdir -p run/jpsi2incl/log/data09
 	   qsub pbs/qsub_jpsi2incl_data09.sh  
 	   ;;
 
-    0.1.26) echo "Check Jpsi2incl PBS jobs on 09 data..."
+    0.1.26) echo "Check Jpsi2incl Condor jobs on 09 data..."
 	   ./python/chk_pbsjobs.py run/jpsi2incl/data09  314 
 	   ;;
     
