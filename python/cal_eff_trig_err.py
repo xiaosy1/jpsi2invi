d45f6787914482fc798eb3e68f93f9b1f6fd29db
Suyu Xiao <1203111345@qq.com>
2018-03-06 20:08:48 +0800

    update

---
+++ python/cal_eff_trig_err.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Calculate trigger efficiency error
+"""
+
+__author__ = "XIAO Suyu<xiaosuyu@ihep.ac.cn>"
+__copyright__ = "Copyright (c) XIAO Suyu"
+__created__ = "[2018-02-06 Tue 15:25]"
+
+import math
+
+n1 = 4212.0
+n2 = 4237.0
+N = 5000.0
+
+eff = n1 / n2
+err = math.sqrt(eff*(1-eff)/N)
+
+print 'trig_eff = %.4f +- %f'  % (eff, err)
